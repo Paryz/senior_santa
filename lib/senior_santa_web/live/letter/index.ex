90dc392686b6f778aa34468cd78a5e4fac6f173f
Kamil Zieliński <paryzowy@gmail.com>
2021-06-09 12:44:48 +0200

    working modal

--- lib/senior_santa_web/live/letter/index.ex
+++ lib/senior_santa_web/live/letter/index.ex
@@ -9,6 +9,34 @@ defmodule SeniorSantaWeb.LetterLive.Index do
     params
     |> Map.get("location", "Warszawa")
     |> Services.Letter.get_all_by_location()
-    |> Result.map(&assign(socket, :letters, &1))
+    |> Result.map(fn letters ->
+      socket
+      |> assign(:letters, letters)
+      |> assign(:letter, List.first(letters))
+      |> assign(
+        :reservation,
+        Ecto.Changeset.change(%SeniorSanta.Reservations.IO.Schemas.Letter{})
+      )
+    end)
+  end
+
+  @impl true
+  def handle_event("select", %{"letter_id" => letter_id}, socket) do
+    {:ok, letter} = Services.Letter.get(letter_id)
+    {:noreply, assign(socket, :letter, letter)}
+  end
+
+  @impl true
+  def handle_event("validate", params, socket) do
+    IO.inspect("validate")
+    IO.inspect(params)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("save", params, socket) do
+    IO.inspect("save")
+    IO.inspect(params)
+    {:noreply, socket}
   end
 end
